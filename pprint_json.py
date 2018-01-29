ae80a3cd20e843710122a91ec9810bc27b666c56
asalex04 <35517950+asalex04@users.noreply.github.com>
2018-01-29 18:52:27 +0400

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -6,10 +6,10 @@ def load_data(filepath):
         return json.loads(myfile.read())
 
 
-def pretty_print_json(data):
-   print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(jstxt):
+    print(json.dumps(jstxt, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    data = load_data(input("path to file: "))
-    pretty_print_json(data)
+    jstxt = load_data(input("path to file: "))
+    pretty_print_json(jstxt)
