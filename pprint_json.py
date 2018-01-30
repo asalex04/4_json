c6d66e0f939591519ed868b3f1e255a3b3302498
asalex04 <35517950+asalex04@users.noreply.github.com>
2018-01-30 14:01:05 +0400

    Update pprint_json.py
    changed way enter  path of the file

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,15 @@
 import json
-
+from sys import argv
 
 def load_data(filepath):
     with open(filepath) as myfile:
         return json.loads(myfile.read())
 
 
-def pretty_print_json(jstxt):
-    print(json.dumps(jstxt, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(json_file):
+    print(json.dumps(json_file, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    jstxt = load_data(input("path to file: "))
-    pretty_print_json(jstxt)
+   
+    pretty_print_json(load_data(argv[1]))
