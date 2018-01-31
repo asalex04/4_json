b57b4335eb8fd7d439ad3cc28d194f3ea8b9c57e
asalex04 <arc04@mail.ru>
2018-01-31 20:22:24 +0400

    change the file name

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,19 @@
 import json
 from sys import argv
+import os
 
 def load_data(filepath):
-    with open(filepath) as myfile:
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath,"r", encoding='utf-8') as myfile:
         return json.loads(myfile.read())
 
 
-def pretty_print_json(json_file):
-    print(json.dumps(json_file, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(data_dictionary):
+    print(json.dumps(data_dictionary, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-   
-    pretty_print_json(load_data(argv[1]))
+
+  
+       pretty_print_json(load_data(argv[1]))
