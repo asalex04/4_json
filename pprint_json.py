abb62158b9ad89ed3c0b4b683a23afbe0da31b38
asalex04 <arc04@mail.ru>
2018-01-31 21:06:13 +0400

    fixed wrong PEP 8

--- pprint_json.py
+++ pprint_json.py
@@ -2,18 +2,19 @@ import json
 from sys import argv
 import os
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath,"r", encoding='utf-8') as myfile:
+    with open(filepath, "r", encoding='utf-8') as myfile:
         return json.loads(myfile.read())
 
 
 def pretty_print_json(data_dictionary):
-    print(json.dumps(data_dictionary, sort_keys=True, indent=4, ensure_ascii=False))
+    print(json.dumps(data_dictionary, sort_keys=True,
+                     indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
 
-  
-       pretty_print_json(load_data(argv[1]))
+    pretty_print_json(load_data(argv[1]))
