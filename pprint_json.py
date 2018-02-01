f91810dc7da4aa70babb07fd861852ab115500d6
asalex04 <arc04@mail.ru>
2018-02-01 11:04:42 +0400

    fix bugs

--- pprint_json.py
+++ pprint_json.py
@@ -4,15 +4,18 @@ import os
 
 
 def load_data(filepath):
-    if not os.path.exists(filepath):
-        return None
-    with open(filepath, "r", encoding='utf-8') as myfile:
-        return json.loads(myfile.read())
+    if os.path.exists(filepath):
+       with open(filepath, 'r', encoding='utf-8') as my_file:
+           return json.loads(my_file.read())
 
 
-def pretty_print_json(data_dictionary):
-    print(json.dumps(data_dictionary, sort_keys=True,
-                     indent=4, ensure_ascii=False))
+def pretty_print_json(json_content):
+    print(json.dumps(
+        json_content,
+        indent=4,
+        sort_keys=True,
+        ensure_ascii=False,
+    ))
 
 
 if __name__ == '__main__':
